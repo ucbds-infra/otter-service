2032bc0b47196fdc09a965fdfcb937f261e7e100
Chris Pyles <cpyles@berkeley.edu>
2020-02-28 14:29:51 -0800

    progress

--- setup_containers.py
+++ setup_containers.py
@@ -3,6 +3,22 @@ import subprocess
 import shutil
 import os
 import yaml
+import docker
+from io import BytesIO
+from jinja2 import Template
+
+CLIENT = docker.from_env()
+
+DOCKERFILE_TEMPLATE = Template("""
+FROM ucbdsinfra/otter-grader
+RUN mkdir /home/notebooks
+ADD {% test_folder_path %} /home{% if test_folder_name != "tests" %}
+RUN mv /home/{% test_folder_name %} /home/tests{% endif %}{% if requirements %}
+ADD {% requirements %} /home
+RUN pip3 install /home/{% requirements_filename %}{% endif %}{% if global_requirements %}
+ADD {% global_requirements %} /home
+RUN pip3 install /home/{% global_requirements_filename %}{% endif %}
+""")
 
 def main():
     repo_path = input("What is the absolute path of your assignments repo? [/home/assignments] ")
@@ -21,7 +37,7 @@ def main():
     with open("/home/.LAST_COMMIT_HASH", "r+") as f:
         last_commit_hash = f.read()
 
-    if last_commit_hash != commit_hash_cmd.stdout:
+    if last_commit_hash == commit_hash_cmd.stdout:
         # write commit hash
         with open("/home/.LAST_COMMIT_HASH", "w+") as f:
             f.write(commit_hash_cmd.stdout)
@@ -37,6 +53,32 @@ def main():
     assignments = config["assignments"]
     ids = [a["assignment_id"] for a in assignments]
     assert len(ids) == len(set(ids)), "Found non-unique assignment IDs in conf.yml"
+    # TODO: check for no assignment id conflicts in db
+
+    # TODO: write to the database
+
+    # TODO: start building docker images
+    for a in assignments:
+        requirements = a["requirements"] if "requirements" in a else ""
+        global_requirements = config["requirements"] if "requirements" in config else ""
+
+        dockerfile = DOCKERFILE_TEMPLATE.render(
+            test_folder_path = a["tests_path"],
+            test_folder_name = os.path.split(a["tests_path"])[1],
+            requirements = requirements,
+            requirements_filename = os.path.split(requirements)[1],
+            global_requirements = global_requirements,
+            global_requirements_filename = os.path.split(global_requirements)[1]
+        )
+
+        new_image = CLIENT.images.build(
+            fileobj=BytesIO(dockerfile.encode("utf-8")), 
+            pull=True, 
+            tag=a["assignment_id"]
+        )
+
+        print("Built Docker image {}".format(new_image.tags))
+
 
 if __name__ == "__main__":
     main()
